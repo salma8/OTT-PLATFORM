e17a75437755ec23d677fb5154cb30a47f9824c9
SalmaSalim <salmasha25898@gmail.com>
2024-05-28 11:56:22 +0530

    added new files

--- ott/app/urls.py
+++ ott/app/urls.py
@@ -1,6 +1,8 @@
+from django.contrib.auth.views import LoginView
 from django.urls import path
-from .views import admin_dashboard
+from .views import admin_dashboard, user_login
 
 urlpatterns = [
-    path('', admin_dashboard, name='admin-dashboard'),
+    path('admindash/', admin_dashboard, name='admin-dashboard'),
+    path('', user_login, name='login')
 ]
--- ott/app/views.py
+++ ott/app/views.py
@@ -1,5 +1,8 @@
+from django.contrib.auth import authenticate, login
+from django.http import HttpResponse
 from django.shortcuts import render
 from .models import Movie, Customer
+from .forms import MyLoginForm
 
 
 def admin_dashboard(request):
@@ -10,3 +13,20 @@ def admin_dashboard(request):
         'customers': customers,
     }
     return render(request, 'dashboard.html', context)
+
+
+def user_login(request):
+    if request.method == 'POST':
+        # we will be getting username and password through post
+        login_form = MyLoginForm(request.POST)
+        if login_form.is_valid():
+            cleaned_data = login_form.cleaned_data
+            auth_user = authenticate(request, username=cleaned_data['username'],
+                                     password=cleaned_data['password'])
+            if auth_user is not None:
+                login(request, auth_user)
+                return HttpResponse('Authenticated')
+    else:
+        login_form = MyLoginForm()
+
+    return render(request, 'login.html', {'login': login_form})
